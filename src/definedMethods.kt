17c88d85d0eefb6e9e8a995c5e26cc1500305e95
Mariga123 <johnmariga8@gmail.com>
2021-08-04 15:31:34 +0300

    assignment

--- src/definedMethods.kt
+++ src/definedMethods.kt
@@ -2,21 +2,24 @@ import java.util.*
 
 fun main(args: Array<String>) {
     var my_scanner = Scanner(System.`in`)
-    println("Enter your name")
+    println("Hello please enter your name")
     var name = my_scanner.next()
 
-    println("Enter the radius")
-    var radius = my_scanner.nextDouble()
+    println("Enter your weight in kgs")
+    var weight = my_scanner.nextDouble()
 
-    println("Enter the height")
+    println("Enter your height in cm")
     var height = my_scanner.nextDouble()
 
-    cyliner_volume_one(name,radius,height)
+    bmi_calc(name,weight,height)
 }
-fun cyliner_volume_one(n:String, r:Double, h:Double){
-    var volume = Math.PI * Math.pow(r,2.0) * h
-    if (volume<10000)
-        println("Hi $n, your volume is $volume and this is a small cylinder")
-    while (volume>10000)
-        println("Hi $n, your volume is $volume and this is a big cylinder")
+fun bmi_calc(n:String, w:Double, h:Double){
+    var bmi = (w /h) * 703.0
+    if (bmi<50.0)
+        println("Hi $n, your BMI is $bmi and you are underweight")
+    else if (bmi>60.0)
+        println("Hi $n, your BMI is $bmi and you have a normal weight")
+    while (bmi<80)
+        println("Hi $n, your BMI is $bmi and you are overweight/obesse")
+
 }
