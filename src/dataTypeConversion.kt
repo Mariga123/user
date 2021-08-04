fcf68f6c21c3446a77efdc306a39b397eddfc095
Mariga123 <johnmariga8@gmail.com>
2021-08-04 14:54:00 +0300

    data type conversion in kotlin

---
+++ src/dataTypeConversion.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    var x:Int
+    var y:Double
+    var a:String
+    var b:String
+
+    x = 10
+    y = 10.0
+    println(x+y)
+    a=x.toString()
+    b=y.toString()
+
+    println(a+b)
+
+    var p = "100"
+    var q = "100"
+    var m : Double
+    var n : Double
+    n = p.toDouble()
+    m= q.toDouble()
+    println(m+n)
+}
